a6d2d6d5bb071686eb09a5ee6b80bced6c59f44a
andakcengiz <spear_cengiz@hotmail.com>
2016-05-22 01:45:19 +0300

    Update TipSearch.php

--- models/TipSearch.php
+++ models/TipSearch.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace backend\modules\yorum\models;
+namespace andakcengiz\yorum\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use backend\modules\yorum\models\Tip;
+use andakcengiz\yorum\models\Tip;
 
 /**
  * TipSearch represents the model behind the search form about `backend\modules\yorum\models\Tip`.
